23f134e196915eb9db448c2167b699147ff21aed
rajdeep.k86 <rajdeep.k86@yahoo.com>
2018-05-27 09:48:29 -0400

    Initial Commit

---
+++ CareerCloud.Pocos/JobEducationPoco.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.Pocos
+{
+    [Table("Job_Educations")]
+
+    public class JobEducationPoco:IPoco
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid Job { get; set; }
+        public string Major { get; set; }
+        public Int16 Importance { get; set; }
+        [Column("Time_Stamp")]
+        public byte[] TimeStamp { get; set; }
+
+    }
+}
